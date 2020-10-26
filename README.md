a5c8adcde33f04820e4318dde7b07e5f80753eec
Alexander Leal <alex.j.leal@live.com>
2020-10-25 22:19:21 -0600

    working

--- README.md
+++ README.md
@@ -5,12 +5,16 @@
 - Create a page that looks like a planner (HTML && CSS)
   +add area with text box
   +add button next to text box to save the textContent (eventListener)
+
 - Store the textContent entered by user
   +local storage
   +textContent
   +save for a specific time
-  -
-- Timer for date
+- Timer for date (moment.js)
   +some way to update date and time on the app
   -3 elementes for each time slot each row is a div (time slot)
   --hint: key is the time && value is the textContent
+
+To Finish
+make the save button work
+save textContent with save button LocalStorage
--- script.js
+++ script.js
@@ -1,2 +1,31 @@
+// var saveBtn = document.addEventListener("click", localStorage)
+
+var input = document.getElementsByTagName("textarea");
+console.log(input);
+// localStorage.setItem("textContent", input.val());
+// console.log(input)
+// let saveInput = [];
+// //TextContent saved here
+// var addText = (ev)=>{
+//     //stop form auto submission & reload on click
+//     ev.preventDefault();
+//     let text = {
+//         id: Date.now(),
+//         title: document.getElementsByTagName("textarea").value,
+//     }
+//     //saves userInput and puts it in an array declared above
+//     saveInput.push(text);
+//     document.getElementsByTagName("textarea").reset();
+//     localStorage.setItem("textContentList", JSON.stringify(saveInput) );
+// }
+
+
+
+// document.getElementById("saveBtn").addEventListener("click", localStorage)
+
+
+
+
+
 //moment.js time tracking
-moment().format('MMMM Do YYYY, h:mm:ss a');
+// moment().format('MMMM Do YYYY, h:mm:ss a');
